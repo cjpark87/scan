4b502cf2465c4c1326298ef7d66243e8091f3783
Junsung Lim <wns349@kaist.ac.kr>
2015-09-30 11:14:23 +0900

    Remove unnecessary menu on SplashActivity

--- app/src/main/java/kr/ac/kaist/nmsl/scan/SplashActivity.java
+++ app/src/main/java/kr/ac/kaist/nmsl/scan/SplashActivity.java
@@ -4,8 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.Window;
 
 public class SplashActivity extends Activity {
@@ -25,27 +23,5 @@ public class SplashActivity extends Activity {
                 finish();
             }
         }, Constants.SPLASH_TIME_OUT);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_splash, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
