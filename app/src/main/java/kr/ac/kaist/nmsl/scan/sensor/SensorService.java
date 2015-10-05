d7f7b28dd94ea841af4f1ec98ece35b8037de79c
Chunjong Park <cjpark87@gmail.com>
2015-10-05 22:35:37 +0900

    Add writeData format for 0 sensor data(i.e. step detection)

--- app/src/main/java/kr/ac/kaist/nmsl/scan/sensor/SensorService.java
+++ app/src/main/java/kr/ac/kaist/nmsl/scan/sensor/SensorService.java
@@ -46,13 +46,34 @@ public class SensorService extends Service implements SensorEventListener {
 
     protected void writeData(SensorEvent event) {
         JSONObject value = new JSONObject();
-        for (int i = 0; i < mDataCount; i++) {
-            try {
-                value.put("event" + i, event.values[i]);
-            } catch (Exception e) {
-                Log.e(Constants.DEBUG_TAG, e.getMessage());
+
+        try {
+            if (mDataCount == 0) {
+                value.put(mSensorTypeName, true);
+            } else {
+                for (int i = 0; i < mDataCount; i++) {
+
+                        switch (i) {
+                            case 0:
+                                value.put("x" + i, event.values[i]);
+                                break;
+                            case 1:
+                                value.put("y" + i, event.values[i]);
+                                break;
+                            case 2:
+                                value.put("z" + i, event.values[i]);
+                                break;
+                            default:
+                                value.put("event" + i, event.values[i]);
+                                break;
+                        }
+
+                }
             }
+        } catch (Exception e) {
+            Log.e(Constants.DEBUG_TAG, e.getMessage());
         }
+
         FileUtil.writeData(this, new Date(), mSensorTypeName, value);
     }
 }
