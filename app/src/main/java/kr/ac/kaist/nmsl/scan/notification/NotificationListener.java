b3efc3ffe6e6ce24482986d45bc0f23f6831a786
Junsung Lim <wns349@kaist.ac.kr>
2015-10-27 16:35:57 +0900

    Display notification icon when listener service is running

--- app/src/main/java/kr/ac/kaist/nmsl/scan/notification/NotificationListener.java
+++ app/src/main/java/kr/ac/kaist/nmsl/scan/notification/NotificationListener.java
@@ -1,19 +1,26 @@
 package kr.ac.kaist.nmsl.scan.notification;
 
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
 import java.util.Set;
 
 import kr.ac.kaist.nmsl.scan.Constants;
+import kr.ac.kaist.nmsl.scan.R;
 import kr.ac.kaist.nmsl.scan.util.SharedPreferenceUtil;
 
 public class NotificationListener extends NotificationListenerService {
     Context context;
+
+    // Sets an ID for the notification
+    private int mNotificationId = 001;
+    private NotificationManager mNotifyMgr;
 
     @Override
     public void onCreate() {
@@ -21,6 +28,20 @@ public class NotificationListener extends NotificationListenerService {
         context = getApplicationContext();
 
         Log.d(Constants.DEBUG_TAG, "Creating NotificationListener");
+
+        NotificationCompat.Builder notiBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(android.R.drawable.ic_menu_view)
+                        .setContentTitle(getString(R.string.app_name))
+                        .setContentText(getString(R.string.running))
+                        .setOngoing(true);
+
+        // Gets an instance of the NotificationManager service
+        mNotifyMgr =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        // Builds the notification and issues it.
+        mNotifyMgr.notify(mNotificationId, notiBuilder.build());
     }
 
     private boolean isWhiteListedPackage(String packageName) {
@@ -45,8 +66,13 @@ public class NotificationListener extends NotificationListenerService {
     }
 
     @Override
-
     public void onNotificationRemoved(StatusBarNotification sbn) {
         Log.i(Constants.DEBUG_TAG, "Notification Removed");
+    }
+
+    @Override
+    public void onDestroy() {
+        mNotifyMgr.cancel(mNotificationId);
+        super.onDestroy();
     }
 }
